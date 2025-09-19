e2a1a68fd2865c0ffef17db776cafd745f2e82fb
FatimaLeonidez <fatimaleonidez@gmail.com>
2025-09-19 15:12:53 -0500

    Update OS project1_main.cpp

--- OS project1_main.cpp
+++ OS project1_main.cpp
@@ -1,17 +1,26 @@
 #include <iostream>
 #include <string>
 using namespace std;
-//hiiiiiiiii
+
 int main(){
     string username, password, new_username, new_password, choice1;
     cout << "Powering on..." << endl;
     cout << "Would you like to log in or sign up?" << endl;
-    cin >> choice1;
-    if (choice1 == "log in"){
+    getline(cin, choice1);
+
+    if (choice1 == "sign up"){
+        cout << "Enter  username: " << endl;
+        getline(cin, new_username);
+        cout << "Enter  password:" << endl;
+        getline(cin, new_password);
+        cout << "(8 charatcers minimimum including a lowercase and uppercase letter, number, and special character): " << endl;
+            
+    }
+    else if (choice1 == "log in"){
         cout << "PLease enter username: " << endl;
-        cin >> username;
+        getline(cin, username);
         cout << "Please enter password" << endl;
-        cin >> password;
+        getline(cin, password);
         if (username == new_username && password == new_password){
             cout << "Entering..." << endl;
         }
@@ -22,4 +31,3 @@ int main(){
     
     return 0;
 
-}
