4fec9fb848ac9c7cbdb7ea8c45abd6531150b1a6
FatimaLeonidez <fatimaleonidez@gmail.com>
2025-09-19 19:02:07 -0500

    Update OS project1_main.cpp

--- OS project1_main.cpp
+++ OS project1_main.cpp
@@ -1,34 +1,28 @@
 #include <iostream>
 #include <string>
+#include "auth.h"
 using namespace std;
 
 int main(){
-    string username, password, new_username, new_password, choice1;
+    string newUsername = "";
+    string newPassword = "";
+    string choice1;
+
     cout << "Powering on..." << endl;
     cout << "Would you like to log in or sign up?" << endl;
     getline(cin, choice1);
 
     if (choice1 == "sign up"){
-        cout << "Enter  username: " << endl;
-        getline(cin, new_username);
-        cout << "Enter  password:" << endl;
-        getline(cin, new_password);
-        cout << "(8 charatcers minimimum and must include a lowercase and uppercase letter, number, and special character): " << endl;
+        sign_up(newUsername, newPassword);
+        log_in(newUsername, newPassword);
             
     }
     else if (choice1 == "log in"){
-        cout << "PLease enter username: " << endl;
-        getline(cin, username);
-        cout << "Please enter password" << endl;
-        getline(cin, password);
-        if (username == new_username && password == new_password){
-            cout << "Entering..." << endl;
+        if (newUsername.empty() || newPassword.empty()){
+            cout << "No account fount. Try to sign up ://" << endl;
         }
-        else{
-            cout << "Wrong >:(" << endl;
-        }
+        
     }
     
     return 0;
-
-
+}
