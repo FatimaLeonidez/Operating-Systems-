fc4dada7dde81032dc151e805ab1900f3ad8102e
Cece-wq <ceciliaduran818@gmail.com>
2025-09-20 23:33:05 -0500

    Update OS project1.cpp

--- OS project1.cpp
+++ OS project1.cpp
@@ -1,3 +1,4 @@
+/* Fatima's first draft
 #include <iostream>
 #include <string>
 using namespace std;
@@ -20,5 +21,56 @@ int main(){
         }
     }
     
+    return 0;
+
+} */
+
+#include <iostream>
+#include <string>
+#include <cctype>
+#include "Auth_Header.h" //comment: Same thing. Don't worry about this. It's only a name change.
+
+using namespace std;
+
+int main() {
+    string newUsername = "";
+    string newPassword = "";
+    string choice1;
+
+    cout << "Powering on..." << endl;
+    cout << "Would you like to log in or sign up?" << endl;
+    getline(cin, choice1);
+
+    if (choice1 == "sign up") {
+
+        std::cout << "Success! Redericting you to the sign up page..." << endl;
+
+        sign_up(newUsername, newPassword);
+
+        if (sign_up(newUsername, newPassword)) {
+            cout << "Account created!" << endl;
+        }
+        else {
+            cout << "Try again! Password must be : (8 characters minimimum including \n a lowercase and uppercase letter, number, and special character): " << endl;
+        }
+
+    }
+    else if (choice1 == "log in") {
+
+        std::cout << "Welcome, now redirecting to the log in page..." << endl;
+
+        log_in(newUsername, newPassword);
+
+        if (newUsername.empty() || newPassword.empty()) {
+            cout << "No account found. Try to sign up ://" << endl;
+        }
+        else {
+            log_in(newUsername, newPassword);
+        }
+    }
+    else {
+        cout << "That's not a choice :(" << endl;
+    }
+
     return 0;
 }
