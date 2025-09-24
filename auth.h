3b91cb9f285701e559111e0914f066aacbf21ce0
Cece-wq <ceciliaduran818@gmail.com>
2025-09-24 14:44:06 -0500

    Update auth.h

--- auth.h
+++ auth.h
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-bool sign_up(string& newUsername, string& newPassword);
-bool log_in(const string& newUsername, const string& newPassword);
+bool sign_up(string& newUsername, string& newPassword); //This function exists to establish the proper conditions to sign up and create an account.
+bool log_in(const string& newUsername, const string& newPassword); //This function establishes the conditions to log in successfully after an account has been created.
 
 #endif
