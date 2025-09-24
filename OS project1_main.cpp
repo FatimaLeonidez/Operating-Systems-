cddd332878f9ee05e1cced476241975d8dc2f4e6
Cece-wq <ceciliaduran818@gmail.com>
2025-09-24 14:40:30 -0500

    Update OS project1_main.cpp

--- OS project1_main.cpp
+++ OS project1_main.cpp
@@ -1,66 +1,60 @@
 #include <iostream>
 #include <string>
 #include <cctype>
-#include "auth.h" //comment: Same thing. Don't worry about this. It's only a name change.
+#include "auth.h" //Connects code to the auth.h file 
 
 using namespace std;
 
 int main() {
-    string newUsername = "";
+    string newUsername = ""; 
     string newPassword = "";
     string choice1;
 
-    cout << "Powering on..." << endl;
-    this_thread::sleep_for(chrono::seconds(3));
-    cout << "Booting up..." << endl;
-    this_thread::sleep_for(chrono::seconds(3));
+    cout << "Powering on..." << endl; //Simulates computer powering on
+    this_thread::sleep_for(chrono::seconds(3)); //This line of code allows for a 3-second long pause to occur before the next line appears.
+    cout << "Booting up..." << endl; //Simulates computer booting up
+    this_thread::sleep_for(chrono::seconds(3)); //3 second pause
 
-    do {
+    do { //This segment of code will loop until certain conditions are met.
         cout << "Would you like to log in, sign up, or terminate the program?" << endl;
         cout << "Press 'x' or 'X' to terminate the program." << endl;
-        getline(cin, choice1);
+        getline(cin, choice1); //Prompts the user to make a choice - log in, sign up, or terminate program
 
-        if (choice1 == "sign up") {
+        if (choice1 == "sign up") { //The "sign up" choice
 
-            std::cout << "Success! Redirecting you to the sign up page..." << endl;
-            this_thread::sleep_for(chrono::seconds(3));
-
-            //sign_up(newUsername, newPassword);
+            std::cout << "Success! Redirecting you to the sign-up page..." << endl; 
+            this_thread::sleep_for(chrono::seconds(3)); //3 second pause
 
             if (sign_up(newUsername, newPassword)) {
-                cout << "Account created!" << endl;
+                cout << "Account created!" << endl; //Message appears if all user information was entered correctly
             }
             else {
-                cout << "Try again! Password must be : (8 characters minimimum including \n a lowercase and uppercase letter, number, and special character): " << endl;
+                cout << "Try again! Password must be : (8 characters minimum including \n a lowercase and uppercase letter, number, and special character): " << endl;
+                //Message appears if the password is not strong enough
             }
 
         }
-        else if (choice1 == "log in") {
+        else if (choice1 == "log in") { //The "log in" choice
 
             std::cout << "Welcome, now redirecting to the log in page..." << endl;
-            this_thread::sleep_for(chrono::seconds(3));
+            this_thread::sleep_for(chrono::seconds(3)); //3 second pause
 
-            log_in(newUsername, newPassword);
+            log_in(newUsername, newPassword); //Calls function in auth.h
 
-            /*if (newUsername.empty() || newPassword.empty()) {
-                cout << "No account found. Try to sign up ://" << endl; This requires more code that we do not have set up yet to store previously created accounts.
-            }
-            else {
-                log_in(newUsername, newPassword); Not needed.
-            }*/
         }
-        else if (choice1 == "x" || choice1 == "X") {
+        else if (choice1 == "x" || choice1 == "X") { //This is the choice to terminate the program
             break;
         }
-        else {
+        else { //If any other input is entered, the following error message will appear.
             cout << "Incorrect input. Try again.\n";
         }
 
-    } while (choice1 != "log in" || choice1 != "sign up");
+    } while (choice1 != "log in" || choice1 != "sign up"); //Do-while condition
 
 
     return 0;
 }
+
 
 
 
