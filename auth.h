0aa064f91a0b358eeb940d8f8852984b04a7cde7
Cece-wq <ceciliaduran818@gmail.com>
2025-09-24 14:28:37 -0500

    Update auth.h

--- auth.h
+++ auth.h
@@ -1,10 +1,11 @@
+#pragma once
+#include <string>
 #ifndef AUTH_H
 #define AUTH_H
 
-#include <string> 
 using namespace std;
 
-void sign_up(string& newUsername, string& newPassword); 
+bool sign_up(string& newUsername, string& newPassword);
 bool log_in(const string& newUsername, const string& newPassword);
 
 #endif
