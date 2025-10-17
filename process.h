8f1ad195960f5edeb7313e9e6b3345e1a5882c93
rdz749 <rdzale4302@gmail.com>
2025-10-17 14:19:51 -0500

    Add process class definition in process.h

---
+++ process.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <string>
+
+using namespace std;
+
+class process
+{
+public:
+
+
+private:
+	int pid;
+	int arrival_time;
+	int burst_time;
+	int remaining_time;
+	int waiting_time;
+	int turnaround_time;
+	int memory_required;
+	string io_operations;
+	string state;
+};
+
