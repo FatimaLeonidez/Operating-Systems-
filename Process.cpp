3bd7825446517f80199c3e19df7dc0fa010fc17f
FatimaLeonidez <fatimaleonidez@gmail.com>
2025-10-17 15:08:14 -0500

    Update Process.cpp

--- Process.cpp
+++ Process.cpp
@@ -7,6 +7,7 @@ Process::Process(int id, int arrival, int burst, int memory, bool io)
   :pid(id), arrival_time(arrival), burst_time(burst), memory_required(memory), io_operations(io){
   state = New
   arrival_time = 0;
+  remaing_time = burst;
   }
 
 void Process::updatedState(ProcessState newState){
