86cfe9b79582aa6a7f6276a600cf7488e913b04f
FatimaLeonidez <fatimaleonidez@gmail.com>
2025-10-17 15:05:45 -0500

    Update Process.cpp

--- Process.cpp
+++ Process.cpp
@@ -6,6 +6,7 @@ using namespace std;
 Process::Process(int id, int arrival, int burst, int memory, bool io)
   :pid(id), arrival_time(arrival), burst_time(burst), memory_required(memory), io_operations(io){
   state = New
+  arrival_time = 0;
   }
 
 void Process::updatedState(ProcessState newState){
