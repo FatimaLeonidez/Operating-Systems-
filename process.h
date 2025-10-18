6033d9374d26e213f70d22546e0b55b39a4de13b
rdz749 <rdzale4302@gmail.com>
2025-10-17 20:06:45 -0500

    Refactor Process class and update ProcessState enum

--- process.h
+++ process.h
@@ -1,34 +1,50 @@
+#pragma once
 #ifndef PROCESS_H
 #define PROCESS_H
 #include <string>
-
+#include <iostream>
 using namespace std;
 
-enum ProcessState{NEW, READY, RUNNING, WAITING, TERMINATED}:
 
-class Process
-{
+enum class ProcessState { // enum class for the different states of a process
+    NEW,
+    READY,
+    RUNNING,
+    WAITING,
+    TERMINATED
+};
 
+class Process {
 private:
-	int pid;
-	int arrival_time;
-	int burst_time;
-	//int priority;
-	int remaining_time;
-	int waiting_time;
-	int turnaround_time;
-	int memory_required;
-	bool io_operations;
-	PrcoessState state;
-
+    int pid;
+    int arrival_time;
+    int burst_time;
+    int remaining_time;
+    int waiting_time;
+    int turnaround_time;
+    ProcessState state;
 
 public:
-	Process(int id, int arrival, int burst, int memory, bool io);
-	void updatedState(ProcessState newState};
-	void executionCycle();
-	int getPID() const{return pid;}
-	int getRemainingTime() const {return remaining_time;}
-	ProcessState getState() const{return state;}
+    Process(int pid, int arrival_time, int burst_time); // constructor for process: its 3 params identify its attributes
+
+
+    void execution_cycle();   // takes count of remaining time by decrementing based off of the process' burst time
+    bool is_complete(); // checks if process is complete. if complete, terminate
+
+    int get_pid(); // getter to retrieve process id
+
+    int get_arrival_time(); // getter to retrieve arrival time
+
+    int get_burst_time();
+    int get_remaining_time();
+
+    int get_waiting_time();
+
+    int get_turnaround_time();
+
+    void set_state(ProcessState updated_state); // sets the new state the process is in. 
+    ProcessState get_state(); // takes in the current state of the process.
+    string display_curr_state(); // prints out current state based on ProcessState.
 };
 
 #endif
