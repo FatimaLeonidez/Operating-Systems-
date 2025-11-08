6075b4314f3333a87f95ec70bf7583d1b401e493
Cece-wq <ceciliaduran818@gmail.com>
2025-11-07 20:07:11 -0600

    Update Process_main.cpp

--- Process_main.cpp
+++ Process_main.cpp
@@ -1,1 +1,5 @@
-
+//Here starts SJF scheduling notes
+//Process1 = pid 1, arrival time 0, burst time 3
+//Process2 = pid 2, arrival time 0, burst time 6
+//Turnaround Time = Completion Time - Arrival Time
+//Waiting Time = Turnaround Time - Burst Time
