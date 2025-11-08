0fcc723f14abd9099264a4b5795ad33a0dc3634c
FatimaLeonidez <fatimaleonidez@gmail.com>
2025-11-07 23:01:46 -0600

    Update FCFS.cpp

--- FCFS.cpp
+++ FCFS.cpp
@@ -1,18 +1,48 @@
-#include <iostream>
-#include <algorithm>
 #include "FCFS.h"
 
-using namespace std;
+FCFS::FCFS(const vector<Process>& proc_list) : processes(proc_list) {}
 
-void FCFS::addProcess(Process* p){
-  queue.puch_back(p);
+void FCFS::run() {
+    cout << "FCFS Scheduling Simulation\n";
+
+    int current_time = 0;
+
+    sort(processes.begin(), processes.end(),
+        [](const Process& a, const Process& b) {
+            return a.get_arrival_time() < b.get_arrival_time();
+        });
+
+    for (auto& p : processes) {
+        if (current_time < p.get_arrival_time()) {
+            current_time = p.get_arrival_time();
+        }
+
+        p.set_state(ProcessState::READY);
+        cout << "\nProcess " << p.get_pid() << " is READY at time " << current_time << endl;
+        this_thread::sleep_for(chrono::seconds(1));
+
+        p.set_state(ProcessState::RUNNING);
+        cout << "Process " << p.get_pid() << " is RUNNING...\n";
+
+        for (int i = 0; i < p.get_burst_time(); ++i) {
+            this_thread::sleep_for(chrono::seconds(1));
+            cout << "  Remaining time: " << p.get_burst_time() - (i + 1) << endl;
+        }
+
+        current_time += p.get_burst_time();
+        p.set_state(ProcessState::TERMINATED);
+
+        cout << "Process " << p.get_pid() << " TERMINATED at time " << current_time << endl;
+        cout << "--------------------------------------\n";
+    }
 }
 
-void FCFS::schedule(){
-  sort(queue.begin(), queue.end(), [](Process* a, Process* b){
-    return a-> getArrivalTime() < b->getArrivalTime();
-});
-
-void FCFS::displayResults(){
-  cout<< "\n The FCFS results here:\n";
+void FCFS::displayResults() {
+    cout << "\n===== FCFS Summary =====\n";
+    for (auto& p : processes) {
+        cout << "PID: " << p.get_pid()
+            << " | Waiting Time: " << p.get_waiting_time()
+            << " | Turnaround Time: " << p.get_turnaround_time()
+            << " | Final State: " << p.display_curr_state() << endl;
+    }
 }
