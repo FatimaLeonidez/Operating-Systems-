53283535d6fedd015746017638a361ca39030092
FatimaLeonidez <fatimaleonidez@gmail.com>
2025-11-07 23:08:43 -0600

    Update FCFS.cpp

--- FCFS.cpp
+++ FCFS.cpp
@@ -2,47 +2,68 @@
 
 FCFS::FCFS(const vector<Process>& proc_list) : processes(proc_list) {}
 
-void FCFS::run() {
-    cout << "FCFS Scheduling Simulation\n";
+void FCFS::computeWaitingTimes() {
+    if (processes.empty()) return;
 
-    int current_time = 0;
+    int waiting_time = 0;
+    processes[0].set_state(ProcessState::READY);
+    processes[0].set_state(ProcessState::RUNNING);
 
-    sort(processes.begin(), processes.end(),
-        [](const Process& a, const Process& b) {
-            return a.get_arrival_time() < b.get_arrival_time();
-        });
-
-    for (auto& p : processes) {
-        if (current_time < p.get_arrival_time()) {
-            current_time = p.get_arrival_time();
-        }
-
-        p.set_state(ProcessState::READY);
-        cout << "\nProcess " << p.get_pid() << " is READY at time " << current_time << endl;
-        this_thread::sleep_for(chrono::seconds(1));
-
-        p.set_state(ProcessState::RUNNING);
-        cout << "Process " << p.get_pid() << " is RUNNING...\n";
-
-        for (int i = 0; i < p.get_burst_time(); ++i) {
-            this_thread::sleep_for(chrono::seconds(1));
-            cout << "  Remaining time: " << p.get_burst_time() - (i + 1) << endl;
-        }
-
-        current_time += p.get_burst_time();
-        p.set_state(ProcessState::TERMINATED);
-
-        cout << "Process " << p.get_pid() << " TERMINATED at time " << current_time << endl;
-        cout << "--------------------------------------\n";
+    for (size_t i = 1; i < processes.size(); ++i) {
+        waiting_time += processes[i - 1].get_burst_time();
+        processes[i].set_state(ProcessState::READY);
+        cout << "Process " << processes[i].get_pid()
+            << " waiting for " << waiting_time << " units\n";
     }
 }
 
-void FCFS::displayResults() {
-    cout << "\n===== FCFS Summary =====\n";
-    for (auto& p : processes) {
-        cout << "PID: " << p.get_pid()
-            << " | Waiting Time: " << p.get_waiting_time()
-            << " | Turnaround Time: " << p.get_turnaround_time()
-            << " | Final State: " << p.display_curr_state() << endl;
+void FCFS::computeTurnaroundTimes() {
+    int cumulative_time = 0;
+    for (size_t i = 0; i < processes.size(); ++i) {
+        cumulative_time += processes[i].get_burst_time();
+        processes[i].set_state(ProcessState::TERMINATED);
+        cout << "Process " << processes[i].get_pid()
+            << " completed at time " << cumulative_time << endl;
     }
+}
+
+void FCFS::displayTableAndAverages() {
+    int n = processes.size();
+    if (n == 0) return;
+
+    vector<int> bt(n), wt(n), tat(n);
+    int total_wt = 0, total_tat = 0;
+
+    // burst tine
+    for (int i = 0; i < n; i++) {
+        bt[i] = processes[i].get_burst_time();
+    }
+
+    wt[0] = 0;
+    for (int i = 1; i < n; i++) {
+        wt[i] = bt[i - 1] + wt[i - 1];
+    }
+
+    // turnaround
+    for (int i = 0; i < n; i++) {
+        tat[i] = bt[i] + wt[i];
+    }
+
+    cout << "\n==============================\n";
+    cout << "Process\tBurst Time\tWaiting Time\tTurnaround Time\n";
+    cout << "==============================\n";
+
+    for (int i = 0; i < n; i++) {
+        total_wt += wt[i];
+        total_tat += tat[i];
+        cout << processes[i].get_pid() << "\t\t"
+            << bt[i] << "\t\t"
+            << wt[i] << "\t\t"
+            << tat[i] << "\n";
+    }
+
+    cout << "==============================\n";
+    cout << "Average Waiting Time = " << (float)total_wt / n << endl;
+    cout << "Average Turnaround Time = " << (float)total_tat / n << endl;
+    cout << "==============================\n";
 }
