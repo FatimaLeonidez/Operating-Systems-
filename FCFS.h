5c58b122493eba22077d5d3a6f97f56ab68dac5f
FatimaLeonidez <fatimaleonidez@gmail.com>
2025-11-07 22:27:31 -0600

    Update FCFS.h

--- FCFS.h
+++ FCFS.h
@@ -1,18 +1,21 @@
+#pragma once
 #ifndef FCFS_H
 #define FCFS_H
 
 #include <vector>
-#include <string>
+#include "Process.h"
+
+using namespace std;
 
 class FCFS {
-public: 
-  void addProcess(process* p);
-  void schedule();
-  void displayResults();
+public:
+	void addProcess(Process* p);
+	void FCFSschedule();
+	void displayResults();
 private:
-  vector<Process> queue;
-  double avgWaitingTime = 0.0;
-  double avg_TurnaroundTime = 0.0;
+	vector<Process*> queue;
+	double avg_waiting_time = 0.0;
+	double avg_turnaround_time = 0.0;
 };
 
 #endif
