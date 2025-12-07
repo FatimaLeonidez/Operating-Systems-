58af372581ad33876e41247648fbe5129cfe6753
Cece-wq <ceciliaduran818@gmail.com>
2025-12-06 21:44:45 -0600

    Create Page.cpp

---
+++ Page.cpp
@@ -0,0 +1,33 @@
+#include "page.h"
+#include <iostream>
+using namespace std;
+
+
+Page::Page(int pageNumber)
+	: pageNumber(pageNumber), frameNumber(-1), valid(false) {}
+
+int Page::getPageNumber() const {
+	return pageNumber;
+}
+
+int Page::getFrameNumber() const {
+	return frameNumber;
+}
+
+bool Page::isValid() const {
+	return valid;
+}
+
+void Page::setFrameNumber(int frame) {
+	frameNumber = frame;
+}
+
+void Page::setValid(bool v) {
+	valid = v;
+}
+
+void Page::display() const {
+	cout << "Page " << pageNumber
+		<< " | Frame: " << frameNumber
+		<< " | Valid: " << valid << endl;
+}
